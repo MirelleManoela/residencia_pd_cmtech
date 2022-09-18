c6afda6b031eac538e2eb5bb834756692d889889
Mirelle Manoela <mirellemanoela14@gmail.com>
2022-09-18 12:11:03 -0300

    Primeira lista de exercicios

---
+++ lista1/ex1/index.js
@@ -0,0 +1,9 @@
+console.log("exercicio 1")
+
+var rua = prompt ("Rua: ");
+var  numero = prompt ("Numero: ");
+var bairro = prompt ("Bairro: ");
+var cidade = prompt ("Cidade: ");
+var uf = prompt ("uf: ");
+
+alert ("O usuário mora em: " +cidade + " / " + uf+  ", no bairro " + bairro+ ", na rua "+ rua + " com numero " + numero + ".");
---
+++ lista1/ex2/index.js
@@ -0,0 +1,4 @@
+console.log("Exercicio 2")
+
+<script> function pares (x, y){ var i; for(i=x; i<=y; i++){ if (i%2 == '0'){ console.log(i); i++; } } }; pares(10, 50);
+alert ("numeros pares: " + i);
